a428134a5fdc1229097e2a59d0201e335123d137
Ivan <ivanfrancisco_20@outlook.com>
2021-11-10 12:35:36 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aplicacion3LeerPorConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un número");
+            int numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce otro número");
+            int numer2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"La suma de los numeros {numero1} y {numer2} es: {numero1 + numer2}");
+        }
+    }
+}
